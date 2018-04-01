8e844cb7d3b6988b7849fe73f6e046a1853355c9
ParaSLee <610033941@qq.com>
2018-04-01 22:50:32 +0800

    更新base

--- config/index.js
+++ config/index.js
@@ -42,8 +42,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/yixiumanage/',
+    assetsSubDirectory: 'yixiumanage',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
