745c67fda135c775597f301d44ca0948c3829e00
ParaSLee <610033941@qq.com>
2018-04-18 21:12:44 +0800

    更新

--- src/views/examine/App.vue
+++ src/views/examine/App.vue
@@ -9,35 +9,50 @@
     </mu-select-field>
 
     <mu-text-field hintText="搜索商户名" v-model="searchText" style="width:200px;"/>
+  </div>
 
+  <div class="stateChoseBox">
+    <datepicker class="choseTime" language="zh" placeholder="选择开始时间" v-model="chosedStartDay" :format="format"></datepicker>
+    <span class="deltime" @click="clearTime('start')">清除</span>
+    <span style="font-size:18px;margin:0 10px;">&nbsp;至</span>
+    <datepicker class="choseTime" language="zh" placeholder="选择结束时间" v-model="chosedEndDay" :format="format"></datepicker>
+    <span class="deltime" @click="clearTime('end')">清除</span>
+  </div>
+  <span class="tips" v-if="showTimetip">{{ timeTips }}</span>
+  <span class="tips errtips" v-if="showTimetipErr">{{ timeTips }}</span>
+
+  <div class="searchBoxErea">
     <mu-raised-button label="搜索" @click="toSearch" class="returnBtn" primary/>
     
     <mu-raised-button label="返回全部" @click="returnAll" v-if="returnAllShow" class="returnBtn" primary/>
   </div>
+
   <p class="payment">
     总保证金：<span>{{ payment }}</span> 元  
     <span class="spm"></span>
     总商户：<span>{{ shopCount }}</span> 家 
   </p>
+
+  
   
   <mu-circular-progress :size="40" v-if="circleShow" class="circleBox"/>
 
   <mu-table enableSelectAll :showCheckbox="false" ref="table" class="listTable" :height="'660px'">
     <mu-thead>
       <mu-tr>
-        <mu-th>ID</mu-th>
         <mu-th>商户名</mu-th>
         <mu-th>联系方式</mu-th>
         <mu-th>创建时间</mu-th>
+        <mu-th>保证金</mu-th>
         <mu-th>商家状态</mu-th>
       </mu-tr>
     </mu-thead>
     <mu-tbody>
       <mu-tr v-for="shop in shopData" :key="shop._id" >
-        <mu-td>{{ shop.id }}</mu-td>
         <mu-td>{{ shop.name }}</mu-td>
         <mu-td>{{ shop.contactNumber}}</mu-td>
         <mu-td>{{ shop.time}}</mu-td>
+        <mu-td>{{ shop.payment/100 }} 元</mu-td>
         <mu-td>
           <span v-if="shop.qualificationState==='待审核'" class="wait">待审核</span>
           <span v-else-if="shop.qualificationState==='未通过'" class="unpass">未通过</span>
@@ -86,11 +101,18 @@
     getShopListAllNumber,
     getVideoData
   } from '../common/api'
+  import Datepicker from 'vuejs-datepicker';
   import Mdialog from "../common/dialog"
 
   export default {
     data(){
       return {
+        chosedStartDay:"",  //开始日期选择
+        chosedEndDay:"",  //结束日期选择
+        format:"yyyy-MM-dd",  //日期格式
+        timeTips:"如果未选择时间节点，则搜索所有时间内的订单",
+        showTimetip: true,
+        showTimetipErr: false,
         loading:true,
         nextpage:true,
         serchstate:"全部",  // 搜索的状态
@@ -103,6 +125,7 @@
         total: 10,    //总页数
         current: 1,   //当前页数
         findshopLish:{
+          collection:"Shop",
           // name: "",
           limit:10,//一次获取列表的条数,系统默认为10
           skip:0//跳过几个数据,系统默认为0
@@ -129,9 +152,17 @@
       }
     },
     components: {
-      Mdialog
+      Mdialog,
+      Datepicker
     },
     methods: {
+      clearTime(type){
+        if (type==="start") {
+          this.chosedStartDay = "";
+        }else if (type==="end"){
+          this.chosedEndDay = "";
+        }
+      },
       //点击页码
       handleClick (newIndex) {
         this.findshopLish.limit = 10*newIndex;
@@ -141,14 +172,17 @@
       //获取10条商家内容
       getShopList (pickData,type){
         this.circleShow = true;
-        getShopListSort(pickData).then(res => {
+        getVideoData(pickData).then(res => {
+          // console.log(res)
           this.listShopData(res, type)
           return {pickData,type};
         },(err => {
           console.log(err)
         })).then((pickData,type) => {
           if (type!=="增加") {
-            this.allDeposit(pickData);
+            this.allDeposit(pickData.pickData);
+          }else{
+            this.circleShow = false;
           }
         })
       },
@@ -163,14 +197,20 @@
         }else{
           delete this.findorderList.name
         }
+        if (type.createdAt!=undefined) {
+          this.findorderList.createdAt = type.createdAt;
+        }else{
+          delete this.findorderList.createdAt
+        }
+        console.log(this.findorderList)
         getVideoData(this.findorderList).then(res => {
-          //console.log(res)
           let allpayment = 0;
           for(let i of res){
             allpayment += i.payment;
           }
           this.payment = allpayment/100;
           this.shopCount = res.length;
+          this.circleShow = false;
         },(err => {
           console.log(err)
         }))
@@ -196,7 +236,6 @@
           this.loading=false;
         }
         // this.delshopList = [];
-        this.circleShow = false;
       },
       //获取商家总数
       getShopNumber (){
@@ -210,8 +249,14 @@
       returnAll(){
         delete this.findshopLish.name;
         delete this.findshopLish.qualificationState;
+        delete this.findshopLish.createdAt;
         this.findshopLish.limit=10;
         this.findshopLish.skip=0;
+        this.chosedStartDay = "";  //开始日期选择
+        this.chosedEndDay = "";  //结束日期选择
+        this.timeTips = "如果未选择时间节点，则搜索所有时间内的订单";
+        this.showTimetip = true;
+        this.showTimetipErr = false;
         this.getShopList(this.findshopLish);
         this.returnAllShow = false;
         this.searchText = "";
@@ -233,8 +278,61 @@
         let type = "增加";
         this.getShopList(this.findshopLish,type)
       },
+      setTimeZero(TimeDate,type){
+        if (type==='start') {
+          TimeDate.setHours(0);
+          TimeDate.setMinutes(0);
+          TimeDate.setSeconds(0);
+          TimeDate.setMilliseconds(0);
+        }else if(type==='end'){
+          TimeDate.setHours(23);
+          TimeDate.setMinutes(59);
+          TimeDate.setSeconds(59);
+          TimeDate.setMilliseconds(0);
+        }
+      },
       //搜索
       toSearch(){
+        let start, end;
+        if (this.chosedStartDay) {
+          this.setTimeZero(this.chosedStartDay,'start');
+          start = this.chosedStartDay/1000;
+        }else{
+          start = "";
+        }
+        if (this.chosedEndDay) {
+          this.setTimeZero(this.chosedEndDay,'end');
+          end = this.chosedEndDay/1000;
+        }else{
+          end = "";
+        }
+        this.findshopLish.createdAt = {};
+
+        if (start === "" && end === "") {
+          delete this.findshopLish.createdAt;
+        }else if (start !== "" && end === "") {
+          this.findshopLish.createdAt = {
+            $gte:start
+          }
+          this.timeTips = `从 ${this.chosedStartDay.getFullYear()}-${this.chosedStartDay.getMonth()+1}-${this.chosedStartDay.getDate()} 至 今`;
+        }else if (start === "" && end !== "") {
+          this.findshopLish.createdAt = {
+            $lte:end
+          }
+          this.timeTips = `从 最初 至 ${this.chosedEndDay.getFullYear()}-${this.chosedEndDay.getMonth()+1}-${this.chosedEndDay.getDate()}`;
+        }else if (start !== "" && end !== "" && start <= end){
+          this.findshopLish.createdAt = {
+            $gte:start,
+            $lte:end
+          }
+          this.timeTips = `从 ${this.chosedStartDay.getFullYear()}-${this.chosedStartDay.getMonth()+1}-${this.chosedStartDay.getDate()} 至 ${this.chosedEndDay.getFullYear()}-${this.chosedEndDay.getMonth()+1}-${this.chosedEndDay.getDate()}`;
+        }else if(start > end){
+          this.showTimetip = false;
+          this.showTimetipErr = true;
+          this.timeTips="开始时间不能大于结束时间！";
+          return 0;
+        }
+
         if (this.serchstate === "全部") {
           delete this.findshopLish.qualificationState;
         }else{
@@ -251,7 +349,7 @@
         this.loading = true;
         this.returnAllShow = true;
         this.nextpage = true;
-        if (this.serchstate === "全部" && this.searchText === "") {
+        if (this.serchstate === "全部" && this.searchText === "" && this.chosedEndDay === "" && this.chosedStartDay === "") {
           this.returnAllShow = false;
         }
       },
@@ -280,7 +378,7 @@
       open (shopData) {
         this.dialog = true;
         this.signalShop = shopData;
-        console.log(this.signalShop);
+        // console.log(this.signalShop);
       },
       //关闭
       close () {
@@ -357,7 +455,6 @@
     margin-top: -51px;
   }
   .returnBtn{
-    margin-left: 10px;
     margin-bottom: 20px;
   }
   .deleteShopBtn{ 
@@ -373,5 +470,38 @@
   }
   .spm{
     margin-right: 30px;
+  }
+  .stateChoseBox{
+    display: flex;
+    flex-direction: row;
+    margin-left: -6px;
+  }
+  .choseTime{
+    margin-left: 7px;
+    margin-bottom: 20px;
+    border-bottom: 3px solid #eee;
+    padding-bottom: 3px;
+    padding-left: 5px;
+  }
+  .deltime{
+    display: inline-block;
+    height: 27px;
+    margin-left: -35px;
+    padding: 1px 5px;
+    z-index: 10;
+    cursor: pointer;
+  }
+  .deltime:hover{
+    background: rgb(229, 229, 229);
+  }
+  .tips{
+    display:inline-block;
+    /*margin-left: 20px;*/
+    font-size: 14px;
+    margin-bottom: 20px;
+    color: rgb(126, 87, 194);
+  }
+  .searchBoxErea{
+    /*margin-top: 10px;*/
   }
 </style>
